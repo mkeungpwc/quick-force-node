11609e2dce2869278c65d714b80d2783b69964e3
mkeungpwc <82924512+mkeungpwc@users.noreply.github.com>
2022-06-29 16:35:40 +0800

    Update app.js

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ app.get('/', function(req, res) {
       // authenticated
       org.authenticate(req.query, function(err) {
         if (!err) {
-          org.query({ query: 'SELECT id, name, type, industry, rating FROM Account' }, function(err, results) {
+          org.query({ query: 'SELECT id, name, type, industry FROM Account' }, function(err, results) {
             if (!err) {
               res.render('index', {records: results.records});
             }
